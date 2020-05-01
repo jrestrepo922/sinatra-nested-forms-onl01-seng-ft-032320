1f6c3605fed7de1dfe61bb5b7655eada177383ef
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-01 21:12:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ module FormsLab
     post '/pirates' do
 
       @pirate = Pirate.new(params[:pirate])
-
+      binding.pry
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
       @ships = Ship.all
 
-      #binding.pry
+
       erb :'pirates/show'
     end
 
