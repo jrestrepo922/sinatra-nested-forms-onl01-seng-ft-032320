1c24049f54203ee5ec4399b1b73c6214b3135cf0
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-01 21:19:13 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(argument)
+    #binding.pry
+    @name = argument[:name]
+    @weight = argument[:weight]
+    @height = argument[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
